85423aad9ae4e325368bdb704de179794b4a961a
devoteeprashant <devoteeprashant@gmail.com>
2024-09-02 18:40:56 +0530

    html css js /  file to select box in js

---
+++ index.js
@@ -0,0 +1,11 @@
+const square =  document.querySelectorAll(".files")
+const square2 =  document.querySelectorAll(".square")
+const filesNameArray = ["a" ,"b" ,"c", "d" ,"e" ,"f", "g" ,"h"];
+
+for( let a of square ){
+    // console.log(a);
+    console.log(a.children);
+}
+// for( let a of square2 ){
+//     console.log(a);
+// }
